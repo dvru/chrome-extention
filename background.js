cbf487d3984f663bdf8c22c654c00109b2ad9678
dvru <damvaru4@gmail.com>
2020-08-19 17:11:11 -0500

    basic structure

---
+++ background.js
@@ -0,0 +1,10 @@
+// checks for which domain they are on
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        if(/^https:\/\/www\.google/.test(current_tab_info.url)){
+            chrome.tabs.insertCSS(null, {file: './styles.css'});
+            chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('I injected'))
+        }
+    })
+});
+
---
+++ foreground.js
@@ -0,0 +1,1 @@
+document.querySelector('#hplogo img').classList.add('spinspin')
