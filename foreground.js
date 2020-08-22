7efd675204fd8d4258ef1b308f266e7cc610aecd
dvru <damvaru4@gmail.com>
2020-08-21 22:42:44 -0500

    basic structure

--- foreground.js
+++ foreground.js
@@ -1,22 +1,22 @@
 document.querySelector('#hplogo img').classList.add('spinspin')
 
-// const first = document.createElement('button');
-// first.innerText = "SET DATA";
-// first.id = "first";
+const first = document.createElement('button');
+first.innerText = "SET DATA";
+first.id = "first";
 
-// const second = document.createElement('button');
-// second.innerText = "im backend";
-// second.id = "second";
+const second = document.createElement('button');
+second.innerText = "im backend";
+second.id = "second";
 
-// document.querySelector('body').appendChild(first)
-// document.querySelector('body').appendChild(second)
+document.querySelector('body').appendChild(first)
+document.querySelector('body').appendChild(second)
 
-// first.addEventListener('click', () => {
-//     chrome.storage.local.set({ "password": "123" })
-//     console.log("set dataaa")
-// });
+first.addEventListener('click', () => {
+    chrome.storage.local.set({ "password": "123" })
+    console.log("set dataaa")
+});
 
-// second.addEventListener('click', () => {
-//     chrome.runtime.sendMessage({message: 'storage!'});
-//     console.log("storagestorage")
-// });
+second.addEventListener('click', () => {
+    chrome.runtime.sendMessage({message: 'storage!'});
+    console.log("storagestorage")
+});
